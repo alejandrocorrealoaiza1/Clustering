86793c4948938a307d9d38b85c5f802c0aaa1c55
alejandrocorrealoaiza1 <alejandro.correa.loaiza1@gmail.com>
2023-04-22 02:29:59 -0500

    Version 1.5

--- Unsupervised/silhoutte.py
+++
@@ -1,5 +0,0 @@
-def silhouette_score_block(X, labels, metric='euclidean', sample_size=None,
-                           random_state=None, n_jobs=1, **kwds):
-    x = (b - a) / max(a, b)
-
-    return x
